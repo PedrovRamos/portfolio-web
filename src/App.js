53167e0ac9e60ab7281057187d881a59a56d3619
PedrovRamos <pvramos.cunha@gmail.com>
2023-01-09 20:45:17 -0300

    Adicionando projeto com erros no console removidos e mensagem no console implementada

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
@@ -5,8 +6,12 @@ import Skills from './components/Skills';
 import Portfolio from './components/Portfolio';
 import ContactMe from './components/ContactMe';
 import Footer from './components/Footer';
+import consoleMessage from './utilities/consoleMessage';
 
 function App() {
+  useEffect(() => {
+    consoleMessage();
+  });
   return (
     <div>
       <Header />
